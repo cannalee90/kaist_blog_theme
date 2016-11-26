858e43c8d2769af7a9b938613984f201bbfd04ad
이강호 (aka. hakgb11) <cannalee90@gmail.com>
2016-11-26 11:55:39 +0000

    first blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'blog/intro'
-
+  get 'blog/new'
+  post 'blog/write'
+  post 'blog/update/:id', to: 'blog#update'
+  get 'blog/show/:id', to: 'blog#show'
+  get 'blog/edit/:id', to: 'blog#edit'
+  root 'blog#list'
+  
 end
