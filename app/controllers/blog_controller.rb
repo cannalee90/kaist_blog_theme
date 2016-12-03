7a33353df6cb75d707c531d4b9ea6570b839ac42
이강호 <cannalee90@gmail.com>
2016-12-04 02:38:01 +0900

    blog controller update

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,19 +1,20 @@
 class BlogController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :new]
   before_action :check_admin, only: [:edit, :new]
-  
+
   def check_admin
+    #만약 유저가 로그인 했거나 내가 정한 어드민 계정일때만 일때만 글을 쓸수 있게 한다.
     if user_signed_in? and current_user.email != 'test@naver.com'
-      redirect_to :back 
+      redirect_to :back
     end
   end
-  
+
   def intro
   end
-  
+
   def new
   end
-  
+
   def write
     @post = Post.new
     @post.title = params[:title]
@@ -21,20 +22,20 @@ class BlogController < ApplicationController
     @post.save
     redirect_to '/'
   end
-  
+
   def list
     @posts = Post.all
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def edit
     authenticate_user!
     @post = Post.find(params[:id])
   end
-  
+
   def update
     @post = Post.find(params[:id])
     @post.update(title: params[:title], content: params[:content])
